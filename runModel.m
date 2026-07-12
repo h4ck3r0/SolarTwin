6e09168fe21911d7d0bbf2bdb8472ce8f3dc9951
Raj Aryan <46929618+h4ck3r0@users.noreply.github.com>
2026-07-12 21:59:36 +0530

    Add files via upload

---
+++ runModel.m
@@ -0,0 +1,12 @@
+function run_model()
+
+model = 'upqc5_7_1_PI_20a_fin_1_1';
+
+disp('=== Starting simulation ===');
+
+simOut = sim(model);
+
+disp('=== Simulation completed successfully ===');
+disp(['Final simulation time: ' num2str(simOut.tout(end))]);
+
+end
